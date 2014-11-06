4e39c9e01faa0647bbc703232e1bcf09bf632d87
Developer <Developer@XMobileBeast>
2014-11-05 22:46:29 -0600

    Update 2

--- configuracion.php
+++ configuracion.php
@@ -2,13 +2,13 @@
 setlocale                   (LC_ALL, 'es_SV.UTF-8');
 date_default_timezone_set   ('America/El_Salvador');
 
-define('ID_SERVIDOR', 'DemoWebPos');
+define('ID_SERVIDOR', 'lamp');
 define('MODO_GLOBAL', 'NORMAL'); // MODOS: [ NORMAL | DOMICILIO ]
 define('USAR_AUT', true); // forzar autorización para CAJA y PEDIDOS
-define('URI_SERVIDOR', '/SERV'); // URI relativa o absoluta hacia el servidor
-define('URI_AUT', '/'); // URI relativa o absoluta hacia el autorizador
+define('URI_SERVIDOR', 'http://lamp/proyectos/restaurante.sv/SERV'); // URI relativa o absoluta hacia el servidor
+define('URI_AUT', ''); // URI relativa o absoluta hacia el autorizador
 
-define('NOMBRE_RESTAURANTE', 'DemoWebPos');
+define('NOMBRE_RESTAURANTE', 'CloudCRP');
 define('SUCURSAL_EMPRESA','Logical SDTS, S.A. de C.V.');
 define('SUCURSAL_DIRECCION', 'Res. Las Arboledas, Colon, La Libertad, El Salvador. '); // direccion de la sucursal
 define('SUCURSAL_TELEFONO', ' +503 2374 5841');
@@ -18,9 +18,9 @@ define('SUCURSAL_TELEFONO', ' +503 2374 5841');
 define('ID_CACHE', "RSV_SQL_" . crc32(ID_SERVIDOR . URI_SERVIDOR) );
 
 define('db__host','localhost');
-define('db__usuario','demowebpos'); // Nombre de usuario de base de datos
+define('db__usuario','cloudcrp'); // Nombre de usuario de base de datos
 define('db__clave','w3SbYLHUfDu5WJA8'); // Clave de base de datos
-define('db__db','demowebpos'); // Base de datos
+define('db__db','cloudcrpbeta'); // Base de datos
 
 $__listado_nodos['todos'] = 'Todas las ordenes';
 $__listado_nodos['pizzas'] = 'Pizzas y entradas horneadas';
